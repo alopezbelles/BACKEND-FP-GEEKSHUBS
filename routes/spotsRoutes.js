32dc717733a916be040ca04441dbedeb546d451c
alopezbelles <alopezbelles@gmail.com>
2023-01-22 21:18:41 +0100

    delete middleware deletemyspot

--- routes/spotsRoutes.js
+++ routes/spotsRoutes.js
@@ -16,7 +16,7 @@ router.post("/newspot", authBearerMiddleware, isValidRole("admin"), SpotsControl
 router.delete("/deletespot/:id", authBearerMiddleware, isValidUser(), SpotsController.deleteSpot);
 router.post("/savespot", authBearerMiddleware, SpotsController.saveSpotByUser);
 router.get("/myspots/:id",  SpotsController.mySpots);
-router.delete("/deletemyspot/:id", authBearerMiddleware,  SpotsController.deleteMySpot);
+router.delete("/deletemyspot/:id",  SpotsController.deleteMySpot);
 
 
 
